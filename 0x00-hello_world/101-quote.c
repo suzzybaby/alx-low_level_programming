cf1e92157128fbbbe3ebebbbab219454d293ba87
Susan Tobi <tobidami50@gmail.com>
2023-07-14 08:52:21 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
-* main - Entry point
-*
-* Return: Always 1 (Succes)
-* */
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
 int main(void)
 {
-	write(2
-			"and that of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	fprintf(stderr, "%s", "and that piece of art is useful\"");
+	fprintf(stderr, "%s", " - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
