c84c8518564243eb6e49f8d08e2f7e7e9aa2e3ea
Susan Tobi <tobidami50@gmail.com>
2023-07-21 11:07:01 -0700

    All Commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
-	int main(void)
+/**
+* main - Print "_putchar" folloed by a new line.
+*
+* Description: You are not allowed to include standard libraries.
+* Return: 0
+*/
+int main(void)
+{
+	char putchar[] = "_putchar";
+	int i = 0;
+
+	while (putchar[i] != '\0')
 	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+		_putchar(putchar[i]);
+		i++;
 	}
+	_putchar ('\n');
+
+	return (0);
+}
